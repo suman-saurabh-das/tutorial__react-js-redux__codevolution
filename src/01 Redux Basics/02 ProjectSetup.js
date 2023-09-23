733655263f4da1792af4a42a4960e62cd781a3dd
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-09-23 11:49:37 +0530

    01 & 02 Introduction to Redux & Project setup

---
+++ src/01 Redux Basics/02 ProjectSetup.js
@@ -0,0 +1,18 @@
+/*
+	[02] Setting up Project Directory
+
+		• Install Node.js - from nodejs.org
+		• Create a project folder (for 1st part of this series)
+			e.g. redux-basics
+		• In terminal Run : npm init --yes | (initializes a package.json file with default settings)
+		• In terminal Run : npm install redux | (adding redux as dependency for our project)
+		• Create a JS file -> index.js and add a log statement
+			
+		In the terminal Run : node "...fullPath/index.js" (we should see the log statement)
+    e.g. node "x:\Workspace\Codebase\05 React\Code Evolution - React and Redux\tutorial__react-js-redux__codevolution\src\01 Redux Basics\02 ProjectSetup.js"
+    OR right click and select Run code.
+
+	NOTE - Since this is a react project, no need to run npm init, just install npm i redux so that we have the redux package and we can start with the code.
+*/
+
+console.log("Redux Tutorial !");
