1d2f8219d0b0f8fc2e11d15c909ec98c9f57416d
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-09-24 13:32:59 +0530

    07 Store in Redux

--- src/01 Redux Basics/04 index.js
+++ src/01 Redux Basics/04 index.js
@@ -1,4 +1,7 @@
-// [04] BASIC REDUX APP
+// [04] - [06] BASIC REDUX APP
+
+const redux = require('redux')
+const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
@@ -27,6 +30,23 @@ const reducer = (state = initialState, action) => {
   }
 }
 
+// create store
+const store = createStore(reducer)
+console.log('Initial state : ', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state : ', store.getState());
+})
+
+// dispatching actions
+store.dispatch(orderCakes())
+store.dispatch(orderCakes())
+store.dispatch(orderCakes())
+
+unsubscribe()
+
+// store.dispatch(orderCake)    // we get an error if we use dispatch after unsubscribing from store
+
 /*
   Action
     Define a string constant that indicates the type of action.
@@ -38,4 +58,16 @@ const reducer = (state = initialState, action) => {
     A function which takes the state and action as parameters and returns the newState.
     We use a switch statement and based on the action type, we modify the object.
     (Make sure to use the spread operator to make a copy of the object before modifying it)
+
+  Store
+    Import redux using require("redux")
+    Create a store using redux.createStore(reducer) passing in the reducer function
+    The store will get the initial state from the reducer function.
+    
+    Register a listener via store.subscribe(listener)
+    listener is function that gets called anytime the state or store changes.
+    Unregister a listener via calling the return function from subscribe(listener)
+
+    To get the state, use store.getState()
+    To modify a state, use store.dispatch(action) passing in the action
 */
