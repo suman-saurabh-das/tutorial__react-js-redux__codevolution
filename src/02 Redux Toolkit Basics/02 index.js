b1585895d318475cbb585606c8892f80a27f7148
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-09-27 12:40:06 +0530

    20 Configuring the store and dispatching actions

---
+++ src/02 Redux Toolkit Basics/02 index.js
@@ -0,0 +1,16 @@
+// [20] Configuring the store and dispatching actions
+
+const store = require('./app/store');
+const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+
+console.log('Initial state ', store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state ', store.getState());
+})
+
+store.dispatch(cakeActions.ordered(1));
+store.dispatch(cakeActions.ordered(1));
+store.dispatch(cakeActions.ordered(1));
+store.dispatch(cakeActions.restocked(3));
+
+unsubscribe();
---
+++ src/02 Redux Toolkit Basics/app/store.js
@@ -0,0 +1,12 @@
+// [20] Configuring the store and dispatching actions
+
+const configureStore = require('@reduxjs/toolkit').configureStore
+const cakeReducer = require('../features/cake/cakeSlice')
+
+const store = configureStore({
+  reducer: {
+    cake: cakeReducer,
+  }
+})
+
+module.exports = store  // default export
